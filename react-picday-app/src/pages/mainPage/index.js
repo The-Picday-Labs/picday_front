2af8d0e1ac795fea5867459c3011a1a66f065baf
ardor924 <ardor924@gmail.com>
2024-06-09 02:22:38 +0900

    Design : MainPage의 더미UI생성 및 Page별 CSS추가

---
+++ react-picday-app/src/pages/mainPage/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const MainPage = () => {
+    return (
+        <div className='d-flex justify-content-center align-items-center'>
+            <h1>준비중..</h1>
+        </div>
+    )
+}
+
+export default MainPage;
