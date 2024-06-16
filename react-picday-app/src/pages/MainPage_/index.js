be664bbd8fa6f1526f9e4d5b651aa9466cf47d8e
unknown <ardor924@gmail.com>
2024-06-16 15:32:17 +0900

    Etc : 깃 커밋 테스트

--- react-picday-app/src/pages/MainPage_/index.js
+++ react-picday-app/src/pages/MainPage_/index.js
@@ -3,10 +3,18 @@ import styled from 'styled-components';
 
 const MainPage = () => {
     return (
-        <div className='d-flex justify-content-center align-items-center'>
-            <h1>준비중..</h1>
-        </div>
+        <Wrapper>
+            <div className='d-flex justify-content-center align-items-center'>
+                <h1>준비중..</h1>
+            </div>
+        </Wrapper>
     )
 }
 
 export default MainPage;
+
+const Wrapper = styled.div`
+  height: 100vw;
+  height: 100vh;
+  display: flex;
+`;
